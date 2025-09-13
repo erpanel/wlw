a5097afda10436e1db2bbec392f53cc93913ad67
erpanel <esterbrsembiring45@gmail.com>
2025-09-13 15:36:07 +0700

    Update installll.sh

--- installll.sh
+++ installll.sh
@@ -8,4 +8,4 @@ echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudf
 # install cloudflared
 sudo apt-get update && sudo apt-get install cloudflared
 # kyg
-sudo cloudflared service install eyJhIjoiOTc0NDA1YmQzZGIyY2Q0NmU0ZDQ4MzZhN2JmN2Q2NDkiLCJ0IjoiZjBkOWYzMGMtMzUzOS00OGQwLWJmODQtYjdjOGIzZTFlOTFmIiwicyI6IlpEbGpPV0UzWmpjdE1ETXhPUzAwWkRkbExUazNNVFl0TXpWaE1EazVOMlpqTW1WayJ9
+sudo cloudflared service uninstall eyJhIjoiOTc0NDA1YmQzZGIyY2Q0NmU0ZDQ4MzZhN2JmN2Q2NDkiLCJ0IjoiZjBkOWYzMGMtMzUzOS00OGQwLWJmODQtYjdjOGIzZTFlOTFmIiwicyI6IlpEbGpPV0UzWmpjdE1ETXhPUzAwWkRkbExUazNNVFl0TXpWaE1EazVOMlpqTW1WayJ9
